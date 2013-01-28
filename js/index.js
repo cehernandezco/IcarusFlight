6b57e6b3463fe7954453c5a214569193040e09d4
cehernandezco <cehernandezco@gmail.com>
2013-01-28 05:18:27 -0800

    cambios de ubicacion de carpetas

---
+++ js/index.js
@@ -0,0 +1,56 @@
+$(document).bind('pageinit',init)
+var segs = 0;
+
+function init(){
+	log("index image");
+	$("#btnlogin").click(verifyUser);
+	$.mobile.defaultPageTransition = 'slide';
+	$.mobile.phonegapNavigationEnabled = "true";
+	$.mobile.page.prototype.options.backBtnText = "Back";
+	verifydb();
+	timer();
+}
+function verifyUser()
+{
+	log($("#login").val());
+	if ($("#login").val() != null && $("#login").val() != "")
+	{
+		log("valid");
+		$.mobile.changePage("./menu.html");
+		//location.href="#pglogin";
+		
+	}else{
+		$.mobile.changePage("./#popupBasic");
+		
+		log("invalid");
+		alert("Invalid");
+	}
+
+
+}
+function verifydb()
+{
+		
+}
+
+function timer(){
+	log(segs);
+	if (segs<4)
+		{
+			segs ++;
+			setTimeout("timer()",1000);
+			}
+	else
+		{
+					
+			location.href="#pglogin";
+
+			}
+	
+}
+
+function log(message)
+{
+	console.log(message);
+	//alert(message);
+}
